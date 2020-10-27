3ab3edf67f38ebecfc153be9d08a85a81a902c03
edenb <eribog@msn.com>
2020-10-27 22:38:29 +0100

    Allow add, modify and delete of users

---
+++ src/views/editUser.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-dialog v-if="user" v-model="showDialog" max-width="500px">
+    <v-card>
+      <v-card-title>
+        <span class="headline px-3">{{ formTitle }}</span>
+        <v-spacer></v-spacer>
+        <template v-if="user.user_id >= 0">
+          <span class="subtitle-1 px-3">User ID: {{ user.user_id }}</span>
+        </template>
+      </v-card-title>
+
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6" md="4">
+              <v-text-field v-model="user.fullname" label="Full Name"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="4">
+              <v-text-field v-model="user.username" label="Username"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="4">
+              <v-text-field v-model="user.email" label="E-mail"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="4">
+              <v-select v-model="user.role" :items="roles" label="Role"></v-select>
+            </v-col>
+            <v-col cols="12" sm="6" md="4">
+              <v-text-field
+                :append-icon="showApiKey ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="showApiKey ? 'text' : 'password'"
+                readonly
+                v-model="user.api_key"
+                label="API key"
+                class="input-group--focused"
+                @click:append="showApiKey = !showApiKey"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="noChange">Cancel</v-btn>
+        <v-btn color="blue darken-1" text @click="changed">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import {apiMixin} from '@/components/mixins/apiMixin';
+export default {
+  name: "EditUser",
+  mixins: [apiMixin],
+  computed: {
+    formTitle: function () {
+      return this.user.user_id < 0 ? 'New User' : 'Edit User'
+    }
+  },
+  data () {
+    return {
+      showDialog: false,
+      showApiKey: false,
+      roles: ['admin', 'manager', 'viewer'],
+      resolve: null,
+      reject: null,
+      user: {}
+    }
+  },
+  methods: {
+    open(orgUser) {
+      this.user = orgUser;
+      this.showApiKey = false;
+      this.showDialog = true;
+      return new Promise((resolve, reject) => {
+        this.resolve = resolve
+        this.reject = reject
+      })
+    },
+    changed() {
+      // Existing users already have an ID
+      if (this.user.user_id >= 0) {
+        this.apiRequest('put', `users/${this.user.user_id}`, this.user)
+          .then(() => {
+            this.resolve(true)
+           })
+          .catch((err) => {
+            // Only throw an error on server errors
+            if(err.response.status >= 500) {
+              this.reject(true)
+            } else {
+              this.resolve(false)
+            }
+          })
+          .finally(() => {
+            this.showDialog = false
+          })
+      } else {
+        this.apiRequest('post', `users`, this.user)
+          .then(() => {
+            this.resolve(true)
+          })
+          .catch((err) => {
+            // Only throw an error on server errors
+            if(err.response.status >= 500) {
+              this.reject(true)
+            } else {
+              this.resolve(false)
+            }
+          })
+          .finally(() => {
+            this.showDialog = false
+          })
+      }
+    },
+    noChange() {
+      this.resolve(false)
+      this.showDialog = false
+    }
+  }
+}
+</script>
--- src/views/users.vue
+++ src/views/users.vue
@@ -1,47 +1,155 @@
 <template>
-  <v-layout child-flex px-3 py-3>
+  <v-container fluid pa-3>
     <v-data-table
       v-model="selected"
       :headers="headers"
       :items="allUsers"
+      :search="search"
       item-key="user_id"
-      :sort-by="['user_id']"
+      :sort-by="['fullname']"
+      show-select
       class="elevation-1"
     >
+      <template v-slot:top>
+        <v-toolbar
+          flat
+          dense
+          color="secondary"
+          dark
+        >
+          <confirm ref="confirm"></confirm>
+          <editUser ref="editUser"></editUser>
+          <v-toolbar-title>Users</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          >
+          </v-text-field> 
+          <v-spacer></v-spacer>
+          <v-btn
+            color="white"
+            fab
+            dark
+            small
+            icon
+            @click="newItem()"
+          >
+            <v-icon dark>
+              mdi-plus
+            </v-icon>
+          </v-btn>
+          <v-btn
+            color="white"
+            fab
+            dark
+            small
+            icon
+            :disabled="selected.length !== 1"
+            @click="editItem()"
+          >
+            <v-icon dark>
+              mdi-pencil
+            </v-icon>
+          </v-btn>
+          <v-btn
+            color="white"
+            fab
+            dark
+            small
+            icon
+            :disabled="selected.length !== 1 || (selected.length == 1 && selected[0].user_id == $store.state.user.user_id)"
+            @click="deleteItem()"
+          >
+            <v-icon dark>
+              mdi-delete
+            </v-icon>
+          </v-btn>
+        </v-toolbar>
+      </template>
     </v-data-table>
-  </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import {apiMixin} from '@/components/mixins/apiMixin';
-  export default {
-    name: "Users",
-    mixins: [apiMixin],
-    created() {
-      this.apiRequest('get', '/users');
-    },
-    computed: {
-      allUsers: function() {
-        return this.response.data
-      }
-    },
-    data () {
-      return {
-        singleSelect: false,
-        selected: [],
-        headers: [
-          {
-            text: 'User ID',
-            align: 'left',
-            value: 'user_id',
-          },
-          { text: 'Username', value: 'username' },
-          { text: 'Full name', value: 'fullname' },
-          { text: 'E-mail', value: 'email' },
-          { text: 'Role', value: 'role' },
-          { text: 'API key', value: 'api_key' },
-        ],
+import {apiMixin} from '@/components/mixins/apiMixin';
+import Confirm from '@/views/confirm.vue';
+import EditUser from '@/views/editUser.vue';
+export default {
+  components: {
+    Confirm,
+    EditUser
+  },
+  name: "Users",
+  mixins: [apiMixin],
+  created() {
+    this.loadTable();
+  },
+  data () {
+    return {
+      allUsers: [],
+      selected: [],
+      headers: [
+        { text: 'Full Name', value: 'fullname' },
+        { text: 'Username', value: 'username' },
+        { text: 'Role', value: 'role' }
+      ],
+      search: '',
+      newUser: {
+        user_id: -1,
+        username: '',
+        fullname: '',
+        email: '',
+        role: 'viewer'
       }
     }
+  },
+  methods: {
+    loadTable () {
+      this.apiRequest('get', `users`)
+        .then((response) => {
+          this.allUsers = response.data
+        })
+        .catch(() => {
+          // Ignore failed (re)loads
+        })
+    },
+    editItem () {
+      this.showDialogUser(this.selected[0]);
+    },
+    newItem () {
+      this.showDialogUser(this.newUser);
+    },
+    deleteItem () {
+      let messageText = [];
+      messageText.push('Are you sure you want to delete the following user?');
+      this.$refs.confirm.open('Delete', messageText, [this.selected[0].fullname],
+        { color: 'red' })
+        .then((confirm) => {
+          if (confirm) {
+            this.apiRequest('delete', `users/${this.selected[0].user_id}`)
+              .then(() => {
+                this.loadTable();
+                this.selected = [];
+              })
+              .catch((err) => {
+                // Do not reload the table on internal server error
+                if(err.response.status < 500) {
+                  this.loadTable();
+                }
+              })
+          }
+        })
+    },
+    showDialogUser (user) {
+      this.$refs.editUser.open(user)
+        .then(() => {
+          this.loadTable();
+        })
+    }
   }
+}
 </script>
