8877ff99a22d0ccb49166c99cc25006eedcedb55
edenb <eribog@msn.com>
2021-09-28 22:02:15 +0200

    Clean up config files

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,6 @@
 module.exports = {
   "lintOnSave": false,
   "runtimeCompiler": true,
-  "configureWebpack": {
-    "devtool": "source-map",
-    "resolve": {
-      "symlinks": false
-    }
-  },
-  "devServer": {
-    "stats": "verbose",
-    //"proxy": {
-    //  "/api": {
-    //    "target": "http://localhost:3000",
-    //    "logLevel": "debug",
-    //    "changeOrigin": true
-    //  }
-    //}
-  },
   "transpileDependencies": [
     "vuetify"
   ]
