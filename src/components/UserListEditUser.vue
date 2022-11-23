c67fee8c44ca79d6eef2c91326afb72409d84337
edenb <eribog@msn.com>
2022-11-23 22:01:28 +0100

    Refactor new user form

--- src/components/UserListEditUser.vue
+++ src/components/UserListEditUser.vue
@@ -1,52 +1,49 @@
 <template>
-  <v-dialog v-if="user" v-model="showDialog" max-width="500px">
+  <v-dialog v-if="user" v-model="showDialog" max-width="800px">
     <v-card>
-      <v-card-title>
-        <span class="text-h5 px-3">{{ formTitle }}</span>
-        <v-spacer />
-        <template v-if="user.user_id >= 0">
-          <span class="text-subtitle-1 px-3">User ID: {{ user.user_id }}</span>
-        </template>
-      </v-card-title>
+      <v-card-item>
+        <v-card-title>{{ formTitle }}</v-card-title>
+        <v-card-subtitle v-if="user.user_id >= 0">
+          User ID: {{ user.user_id }}
+        </v-card-subtitle>
+      </v-card-item>
 
       <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field v-model="user.username" label="Username" />
-            </v-col>
-            <v-col v-if="user.user_id < 0" cols="12" sm="6" md="4">
-              <v-text-field
-                v-model="user.password"
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                :type="showPassword ? 'text' : 'password'"
-                label="Password"
-                class="input-group--focused"
-                @click:append="showPassword = !showPassword"
-              />
-            </v-col>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field v-model="user.fullname" label="Full Name" />
-            </v-col>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field v-model="user.email" label="E-mail" />
-            </v-col>
-            <v-col cols="12" sm="6" md="4">
-              <v-select v-model="user.role" :items="roles" label="Role" />
-            </v-col>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field
-                v-model="user.api_key"
-                :append-icon="showApiKey ? 'mdi-eye' : 'mdi-eye-off'"
-                :type="showApiKey ? 'text' : 'password'"
-                readonly
-                label="API key"
-                class="input-group--focused"
-                @click:append="showApiKey = !showApiKey"
-              />
-            </v-col>
-          </v-row>
-        </v-container>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field v-model="user.username" label="Username" />
+          </v-col>
+          <v-col v-if="user.user_id < 0" cols="12" sm="6">
+            <v-text-field
+              v-model="user.password"
+              :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPassword ? 'text' : 'password'"
+              label="Password"
+              class="input-group--focused"
+              @click:append-inner="showPassword = !showPassword"
+            />
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field v-model="user.fullname" label="Full Name" />
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field v-model="user.email" label="E-mail" />
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-select v-model="user.role" :items="roles" label="Role" />
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              v-model="user.api_key"
+              :append-inner-icon="showApiKey ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showApiKey ? 'text' : 'password'"
+              readonly
+              label="API key"
+              class="input-group--focused"
+              @click:append-inner="showApiKey = !showApiKey"
+            />
+          </v-col>
+        </v-row>
       </v-card-text>
 
       <v-card-actions>
@@ -57,8 +54,12 @@
           </div>
         </template>
         <v-spacer />
-        <v-btn color="blue-darken-1" variant="text" @click="noChange"> Cancel </v-btn>
-        <v-btn color="blue-darken-1" variant="text" @click="changed"> Save </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="noChange">
+          Cancel
+        </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="changed">
+          Save
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
