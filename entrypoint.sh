b6a967c76fd3a46a1b47342506f6f8c7bc42a694
edenb <eribog@msn.com>
2023-02-19 21:35:12 +0100

    Fix names of environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ echo "Replacing env constants in JS"
 for file in $ROOT_DIR/assets/*.js*;
 do
 
-  sed -i 's|VITE_SERVER_URL|'${VITE_SERVER_URL}'|g' $file
-  sed -i 's|VITE_API_PATH|'${VITE_API_PATH}'|g' $file
+  sed -i 's|$VITE_SERVER_URL|'${VITE_SERVER_URL}'|g' $file
+  sed -i 's|$VITE_API_PATH|'${VITE_API_PATH}'|g' $file
 
 done
 
