05aab089f235a0a5dbb2aeefbcacf0ffe216b2ba
edenb <eribog@msn.com>
2023-07-01 22:37:58 +0200

    Use composition api

--- src/components/ConfirmDialog.vue
+++ src/components/ConfirmDialog.vue
@@ -6,8 +6,8 @@
     @keydown.esc="cancel"
   >
     <v-card>
-      <v-toolbar dark :color="options.color" dense flat>
-        <v-toolbar-title class="text-white">
+      <v-toolbar :color="options.color" density="comfortable">
+        <v-toolbar-title>
           {{ title }}
         </v-toolbar-title>
       </v-toolbar>
@@ -19,86 +19,54 @@
         {{ message }}
       </v-card-text>
       <v-card-text>
-        <v-chip-group column>
-          <v-chip v-for="item in items" :key="item">
-            {{ item }}
-          </v-chip>
-        </v-chip-group>
+        <v-chip v-for="item in items" :key="item" class="ma-1">
+          {{ item }}
+        </v-chip>
       </v-card-text>
       <v-card-actions class="pt-0">
         <v-spacer />
-        <v-btn color="primary-darken-1" variant="text" @click="agree">
-          Yes
+        <v-btn color="blue-darken-1" variant="text" @click="cancel">
+          Cancel
         </v-btn>
-        <v-btn color="grey" variant="text" @click="cancel"> Cancel </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="agree"> Yes </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
-<script>
-/**
- * Vuetify Confirm Dialog component
- *
- * Insert component where you want to use it:
- * <confirm ref="confirm"></confirm>
- *
- * Call it:
- * this.$refs.confirm.open('Delete', 'Are you sure?', { color: 'red' }).then((confirm) => {})
- * Or use await:
- * if (await this.$refs.confirm.open('Delete', 'Are you sure?', { color: 'red' })) {
- *   // yes
- * }
- * else {
- *   // cancel
- * }
- *
- * Alternatively you can place it in main App component and access it globally via this.$root.$confirm
- * <template>
- *   <v-app>
- *     ...
- *     <confirm ref="confirm"></confirm>
- *   </v-app>
- * </template>
- *
- * mounted() {
- *   this.$root.$confirm = this.$refs.confirm.open
- * }
- */
-export default {
-  data: () => ({
-    dialog: false,
-    resolve: null,
-    reject: null,
-    messages: [],
-    items: [],
-    title: null,
-    options: {
-      color: "primary",
-      width: 290,
-      zIndex: 1200,
-    },
-  }),
-  methods: {
-    open(title, messages, items, options) {
-      this.dialog = true;
-      this.title = title;
-      this.messages = messages;
-      this.items = items;
-      this.options = Object.assign(this.options, options);
-      return new Promise((resolve, reject) => {
-        this.resolve = resolve;
-        this.reject = reject;
-      });
-    },
-    agree() {
-      this.resolve(true);
-      this.dialog = false;
-    },
-    cancel() {
-      this.resolve(false);
-      this.dialog = false;
-    },
-  },
-};
+<script setup>
+import { ref } from "vue";
+
+defineExpose({ open });
+const dialog = ref(false);
+const items = ref([]);
+const messages = ref([]);
+const options = ref({
+  color: "primary",
+  width: 290,
+  zIndex: 1200,
+});
+let resolve = null;
+const title = ref(null);
+
+function open(dialogTitle, dialogMessages, dialogItems, dialogOptions) {
+  dialog.value = true;
+  title.value = dialogTitle;
+  messages.value = dialogMessages;
+  items.value = dialogItems;
+  options.value = Object.assign(options.value, dialogOptions);
+  return new Promise((openResolve) => {
+    resolve = openResolve;
+  });
+}
+
+function agree() {
+  resolve(true);
+  dialog.value = false;
+}
+
+function cancel() {
+  resolve(false);
+  dialog.value = false;
+}
 </script>
