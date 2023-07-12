2ccf269e00b27a5078f2dbce1cedd5079256f150
edenb <eribog@msn.com>
2023-07-12 21:21:45 +0200

    Validate longitude and latitude input fields

--- src/forms/rules.js
+++ src/forms/rules.js
@@ -1,4 +1,8 @@
 export const rules = {
+  between(min, max) {
+    return (v) =>
+      (v > min && v < max) || `Only numbers between ${min} and ${max} allowed`;
+  },
   emptyOrEmail: (v) =>
     /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/.test(
       v,
@@ -8,6 +12,7 @@ export const rules = {
   min(minLength) {
     return (v) => v.length >= minLength || `At least ${minLength} characters`;
   },
+  number: (v) => !isNaN(v) || "Field should be a number",
   required: (v) => !!v || "Field is required",
   zeroOrMin(minLength) {
     return (v) =>
